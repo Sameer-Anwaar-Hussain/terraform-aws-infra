5821eb5552246ed9429b85a9f411e6ae7f95d48d
Sameer-Anwaar-Hussain <sameeranwaar12@gmail.com>
2025-05-06 18:28:51 +0400

    Required outputs updated

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,11 @@
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
